9009ae46ec8ffe50a0122ecca0d115593dea0165
Oleksandr Arseniuk <korol1511@gmail.com>
2021-10-19 10:55:14 +0300

    Внесены правки в такс 1 и 2, (3й таск в процессе)

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,22 @@
-'use strict';
+// 'use strict';
 
-const totalCategories = document.querySelectorAll('.item');
-console.log(
-    `Number of categories: ${totalCategories.length}`);
+// const totalCategories = document.querySelectorAll('.item');
+// console.log(`Number of categories: ${totalCategories.length}`);
   
-const categoriesTitle = [...totalCategories].map(
-    categories =>
-      `Category: ${categories.children[0].textContent}, 
-    Elements: ${categories.children[1].children.length}`,
-  )
-  .join('\n');
-console.log(categoriesTitle);
+// const categoriesTitle = [...totalCategories].map(
+//     categories =>
+//       `Category: ${categories.children[0].textContent}, 
+//     Elements: ${categories.children[1].children.length}`,
+//   )
+//   .join('\n');
+// console.log(categoriesTitle);
+
+const totalCategories = document.querySelector("#categories");
+console.log(`Number of categories: ${totalCategories.children.length}`);
+
+const categoriesTitle = document.querySelectorAll(".item")
+
+categoriesTitle.forEach((categories) => {
+  console.log(`Category: ${categories.firstElementChild.textContent}
+  Elements: ${categories.lastElementChild.childElementCount}`);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -7,13 +7,23 @@ const ingredients = [
   'Condiments',
 ];
 
+// let ingredientsNew = document.getElementById("ingredients");
+// console.log(ingredientsNew);
+// let ingredientsNewElement = ingredients.forEach((ingredient) => {
+//   let vegetables = document.createElement("li");
+//   vegetables.innerText = ingredient;
+//   vegetables.classList.add("item");
+//   ingredientsNew.append(vegetables);
+// });
+
 let ingredientsNew = document.getElementById("ingredients");
 console.log(ingredientsNew);
-let ingredientsNewElement = ingredients.forEach((ingredient) => {
-  let vegetables = document.createElement("li");
-  vegetables.innerText = ingredient;
-  vegetables.classList.add("item");
-  ingredientsNew.append(vegetables);
 
-
+let ingredientsNewElement = [];
+ingredients.forEach((ingredient) => {
+  const liItems = document.createElement("li");
+  liItems.classList.add("item");
+  liItems.textContent = ingredient;
+  ingredientsNewElement.push(liItems);
+  ingredientsNew.append(...ingredientsNewElement);
 });
